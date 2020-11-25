36967854ec9ffe0cf4aed3d5b986243343a970f3
alex <aryazanov@eyeota.com>
2020-11-25 15:18:57 +0800

    algorithm analysis for Divisors class

--- com.alexrnv.algobook.math/src/main/java/com/alexrnv/algobook/math/algebra/fundamentals/factorization/Divisors.java
+++ com.alexrnv.algobook.math/src/main/java/com/alexrnv/algobook/math/algebra/fundamentals/factorization/Divisors.java
@@ -6,45 +6,123 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * All the tasks related to number factorization, with analysis.
+ * Links:
+ * https://en.wikipedia.org/wiki/Prime_omega_function
+ * https://en.wikipedia.org/wiki/Prime-counting_function
+ * https://en.wikipedia.org/wiki/Divisor_function#Approximate_growth_rate
+ * http://ramanujan.sirinudi.org/Volumes/published/ram35.pdf
+ *
+ */
 public class Divisors {
 
-    //O(log(sqrt((n))) space
-    //O(sqrt(n)) time = sqrt(n) to get primes + log(n) to iterate primes
+    /**
+     * Returns the list of all factors of n (with repetitions). <br/>
+     * Uses the fact that we only need to examine primes in the range [2; ⎣√n⎦].  <br/><br/>
+     *
+     * Algorithm analysis: <br/>
+     * Time complexity O(√n*log(log(√n)): <br/>
+     * <ol>
+     *     <li> O(√n*log(log(√n)) - to calculate primes from 0 to √n (See {@link Primes#primes(long)}). </li>
+     *     <li> O(√n/log(√n)) - to iterate over primes and collect results. <sup>1</sup> </li>
+     * </ol>
+     * In case of pre-calculated primes time complexity could be reduced O(√n). <br/><br/>
+     * Space complexity O(√n*log(log(√n))): <br/>
+     * <ol>
+     *     <li>O(√n/log(√n)) - to store a list of primes below √n <sup>1</sup></li>
+     *     <li>O(√n*log(log(√n))) - the upper bound to the number of divisors in result array <sup>2</sup></li>
+     * </ol>
+     * <br/>
+     * <sup>1</sup> - see <a href="https://en.wikipedia.org/wiki/Prime-counting_function">Prime-counting function</a> <br/>
+     * <sup>2</sup> - also known as Ω(n), see <a href="https://en.wikipedia.org/wiki/Divisor_function#Approximate_growth_rate">Robin's inequality</a>
+     *
+     * @see Primes#primes(long)
+     */
     public static List<Long> factorsAll(long n) {
         if (n == 1) return Collections.singletonList(1L);
-        List<Long> primes = Primes.primes((int) (Math.sqrt(n)));
-        List<Long> result = new ArrayList<>();
+        int bound = (int) (Math.sqrt(n));
+        List<Long> primes = Primes.primes(bound);
+        // for huge numbers the factor will exceed 2, so the array must be reallocated
+        // in majority of situations, the factor of 2 is a very safe estimate
+        List<Long> result = new ArrayList<>(bound * 2);
         for (long prime : primes) {
+            if (prime > n)
+                break;
             while (n % prime == 0) {
                 n /= prime;
                 result.add(prime);
             }
         }
-        if (n > 1) result.add(n);
+        if (n > 1)
+            result.add(n);
+
         return result;
     }
 
-    //O(log(sqrt((n))) space
-    //O(sqrt(n)) time = sqrt(n) to get primes + log(n) to iterate primes
+    /**
+     * Returns the list of unique factors of n (no repetitions). <br/>
+     * As an optimization, the factor of 2 is processed separately.
+     * The number is divided by 2 until it is odd, yielding smaller number in many cases, and
+     * a smaller number of primes to calculate. <br/>
+     *
+     * Algorithm analysis: <br/>
+     * Time complexity O(√n*log(log(√n)): <br/>
+     * <ol>
+     *     <li> O(√n*log(log(√n)) - to calculate primes from 0 to √n (See {@link Primes#primes(long)}). </li>
+     *     <li> O(√n/log(√n)) - {@link Divisors#factorsUnique(long, java.util.List, java.util.List)}. <sup>1</sup> </li>
+     * </ol>
+     * In case of pre-calculated primes time complexity could be reduced to O(√n). <br/><br/>
+     * Space complexity O(√n/log(log(√n))): <br/>
+     * <ol>
+     *     <li>O(√n/log(√n)) - to store a list of primes below √n <sup>1</sup></li>
+     *     <li>O(log(√n)/log(log(√n))) - the upper bound to the number of unique divisors in result array <sup>2</sup></li>
+     * </ol>
+     * <br/>
+     * <sup>1</sup> - see <a href="https://en.wikipedia.org/wiki/Prime-counting_function">Prime-counting function</a> <br/>
+     * <sup>2</sup> - also known as ω(n), see <a href="http://ramanujan.sirinudi.org/Volumes/published/ram35.pdf">Ramanujan's work</a>
+     *
+     * @see Divisors#factorsUnique(long, java.util.List, java.util.List)
+     */
     public static List<Long> factorsUnique(long n) {
         if (n == 0) return Collections.emptyList();
         if (n == 1) return Collections.singletonList(1L);
-        List<Long> result = new ArrayList<>();
+
+        boolean add2 = false;
         if ((n&1) == 0) {
-            result.add(2L);
+            add2 = true;
+            n = n >> 1;
             while((n&1) == 0) {
                 n = n >> 1;
             }
         }
-        List<Long> primes = Primes.primes(n);
+
+        int bound = (int) (Math.sqrt(n));
+        // for huge numbers the factor will exceed 2, so the array must be reallocated
+        // in majority of situations, the factor of 2 is a very safe estimate
+        // +1 element for prime number 2
+        List<Long> result = new ArrayList<>(bound * 2 + 1);
+        if (add2)
+            result.add(2L);
+
+        List<Long> primes = Primes.primes(bound);
         factorsUnique(n, primes, result);
         return result;
     }
 
-    //O(sqrt(n)) time = log(n) to iterate primes
-    public static void factorsUnique(long n, List<Long> primes, List<Long> result) {
+    /**
+     * Adds only unique factors of n from the provided list of primes into result list. <br/>
+     * Uses the fact that we only need to examine primes in the range [2; ⎣√n⎦].  <br/><br/>
+     * For complexity analysis refer to {@link Divisors#factorsUnique(long)} <br/>
+     *
+     * @see Divisors#factorsUnique(long)
+     * @see Primes#primes(long)
+     */
+    private static void factorsUnique(long n, List<Long> primes, List<Long> result) {
         if (n == 1) return;
         for (long prime : primes) {
+            if (prime > n)
+                break;
             if (n % prime == 0) {
                 n /= prime;
                 result.add(prime);
@@ -57,8 +135,12 @@ public class Divisors {
             result.add(n);
     }
 
-    //O(sqrt(n)) time = log(n) to iterate primes
-    public static int numFactorsUnique(long n, List<Long> primes) {
+    /**
+     * Similar to {@link Divisors#factorsUnique(long, java.util.List, java.util.List)}, but only stores the number of results
+     * instead of the list.
+     * @see Divisors#factorsUnique(long, java.util.List, java.util.List)
+     */
+    private static int numFactorsUnique(long n, List<Long> primes) {
         if (n == 1) return 1;
         int result = 0;
         for (long prime : primes) {
@@ -78,6 +160,16 @@ public class Divisors {
 
     //O(log(sqrt((n))) space
     //O(sqrt(n)) time = sqrt(n) to get primes + log(n) to iterate primes
+
+    /**
+     * Returns the number of divisors of n, including n itself. <br/>
+     * Uses idea described <a href="https://cp-algorithms.com/algebra/divisors.html">here</a> to count the number of
+     * divisors without storing them. <br/>
+     * Check {@link Divisors#factorsUnique(long)} for complexity analysis.
+     *
+     * @see Divisors#factorsUnique(long)
+     * @see Divisors#sumOfDivisors(long)
+     */
     public static int numberOfDivisors(long n) {
         if (n < 0) n = -n;
         if (n == 0) return 0;
@@ -107,7 +199,13 @@ public class Divisors {
     }
 
     /**
-     * @return sum of divisors including the number itself
+     * Returns the sum of divisors of n, including n itself. <br/>
+     * Uses idea described <a href="https://cp-algorithms.com/algebra/divisors.html">here</a> to count the number of
+     * divisors without storing them. <br/>
+     * Check {@link Divisors#factorsUnique(long)} for complexity analysis.
+     *
+     * @see Divisors#factorsUnique(long)
+     * @see Divisors#numberOfDivisors(long)
      */
     public static long sumOfDivisors(long n) {
         if (n < 0) n = -n;
